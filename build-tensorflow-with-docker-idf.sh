e37e5e17a317c09438ed90021e58cfd11e37d834
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-01-06 00:23:36 -0500

    Add customized esp32 makefile
    Go to micropython/ports/esp32 then make -f <path to GNUmakefile> V=1 all
    
    Cleanup unused build scripts.

--- build-tensorflow-with-docker-idf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-winpty docker run -i -t -v /$(pwd)/tensorflow:/tensorflow espressif/idf:v4.0.2 bash
-
--- update-microlite.sh
+++ update-microlite.sh
@@ -1,42 +1,7 @@
 #!/bin/bash
 
-Copy () {
-
-	BASE_DIR=$1
-
-	find $BASE_DIR -iname *.h | grep -v make\/downloads | grep -v examples | while read FP; do
-
-		BASE_FP=$(basename $FP)
-		DIR_FP=$(dirname $FP)
-
-
-		mkdir -p include/$DIR_FP
-		cp $FP include/$DIR_FP
-		echo "cp $FP include/$DIR_FP"
-
-	done
-
-
-
-}
-
 TF_LITE=tensorflow/tensorflow/lite
 TF_LITE_MICRO=$TF_LITE/micro
 
 cp $TF_LITE_MICRO/tools/make/gen/esp_xtensa-esp32/lib/libtensorflow-microlite.a lib
-
-rm -rf include
-
-Copy $TF_LITE_MICRO
-
-cp $TF_LITE/*.h include/$TF_LITE
-
-
-Copy "$TF_LITE/c"
-Copy "$TF_LITE/core"
-Copy "$TF_LITE/schema"
-Copy "$TF_LITE/kernels"
-
-Copy "$TF_LITE_MICRO/tools/make/downloads/flatbuffers/include"
-
 
