aff88946d11b0d423ee62a42171930e9b568dbae
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-01-16 00:00:31 -0500

    Changes to build the unix port

--- build-with-unix-docker.sh
+++ build-with-unix-docker.sh
@@ -3,7 +3,8 @@
 DOCKER_IMAGE=$1
 
 if test -z "$DOCKER_IMAGE"; then
-	DOCKER_IMAGE=madduci/docker-linux-cpp
+	# need to build the docker/unix-build image first
+	DOCKER_IMAGE=unix-build
 fi
 
 winpty docker run -i -t -v /$(pwd):/src  --entrypoint "bash" $DOCKER_IMAGE
