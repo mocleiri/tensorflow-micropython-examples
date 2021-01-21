e456c2b400446eb7b9d711c8d3b1d01f7b01ec0e
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-01-20 23:40:06 -0500

    Changes to the hello-world example to generate more data
    I changed it so that the functions can adjust a global variable
    and then increment that value up which will
    then emit y values that can be plotted.
    
    I plotted the output and confirmed the approximated sine wave

--- src/hello-world/hello-world.py
+++ src/hello-world/hello-world.py
@@ -1,11 +1,19 @@
+# Microlite implementation of the tensorflow hello-world example
+# https://github.com/tensorflow/tensorflow/tree/master/tensorflow/lite/micro/examples/hello_world
 import microlite
 
 counter = 1
 
 kXrange = 2.0 * 3.14159265359
 
+current_input = None
+
+
 def input_callback (microlite_interpreter):
-    print ("input callback")
+
+    global current_input
+
+    # print ("input callback")
     # can't print the tensor directly because it is not an mp_obj_t
     # we probably need to define a container object that will hold the TfLiteTensor pointer
     # we may be able to put the pointer directly as a field in the interpreter class
@@ -13,7 +21,7 @@ def input_callback (microlite_interpreter):
 
     inputTensor = microlite_interpreter.getInputTensor(0)
 
-    print (inputTensor)
+    # print (inputTensor)
 
     position = (counter*1.0) / 1.0
 
@@ -21,27 +29,32 @@ def input_callback (microlite_interpreter):
 
     x = position * kXrange
 
-    print ("x: %f" % x)
+    current_input = x
+    # print ("x: %f, " % x)
 
     x_quantized = inputTensor.quantizeFloatToInt8(x)
 
     inputTensor.setValue(0, x_quantized)
 
 def output_callback (microlite_interpreter):
-    print ("output callback")
+    global current_input
+    # print ("output callback")
 
     outputTensor = microlite_interpreter.getOutputTensor(0)
 
-    print (outputTensor)
+    # print (outputTensor)
 
     y_quantized = outputTensor.getValue(0)
     
     y = outputTensor.quantizeInt8ToFloat(y_quantized)
-    
-    print ("y = %f" % y)
+
+    print ("%f,%f" % (current_input,y))
 
 
 
 interp = microlite.interpreter(2048, input_callback, output_callback)
 
-interp.invoke()
+print ("time step,y")
+for c in range(1000):
+    interp.invoke()
+    counter = counter + 1
