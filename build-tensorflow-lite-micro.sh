da2e442e0dba45cb26ce96bfbe79cdc40c1bdbd5
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2021-05-09 22:34:25 -0400

    Turn off debugging when building tensorflow

--- build-tensorflow-lite-micro.sh
+++ build-tensorflow-lite-micro.sh
@@ -4,7 +4,7 @@ cd tensorflow
 
 # These CFLAGS came from looking at what the Micropython c++ build was emitting.
 make -f tensorflow/lite/micro/tools/make/Makefile \
-CXXFLAGS="-DNDEBUG -std=gnu++11 -ffunction-sections -fdata-sections -fstrict-volatile-bitfields -mlongcalls -nostdlib -Wall -Werror -Wno-error=unused-function -Wno-error=unused-but-set-variable -Wno-error=unused-variable -Wno-error=deprecated-declarations -DESP_PLATFORM  -Wno-error=sign-compare -DMICROPY_ESP_IDF_4=1 -c -MD -DTF_LITE_STATIC_MEMORY -DTF_LITE_DISABLE_X86_NEON -Wsign-compare -Wdouble-promotion -Wshadow -Wvla -O3 -Wno-nonnull" \
+CXXFLAGS="-std=gnu++11 -Os -DNDEBUG -ffunction-sections -fdata-sections -fstrict-volatile-bitfields -mlongcalls -nostdlib -Wall -Werror -Wno-error=unused-function -Wno-error=unused-but-set-variable -Wno-error=unused-variable -Wno-error=deprecated-declarations -DESP_PLATFORM  -Wno-error=sign-compare -DMICROPY_ESP_IDF_4=1 -c -MD -DTF_LITE_STATIC_MEMORY -DTF_LITE_DISABLE_X86_NEON" \
 TARGET_TOOLCHAIN_PREFIX=xtensa-esp32-elf- TARGET=esp32 TARGET_ARCH=xtensa-esp32 $@
 
 #tensorflow/lite/micro/tools/make/gen/esp32_xtensawin/lib/libtensorflow-microlite.a
