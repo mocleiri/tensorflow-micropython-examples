9e2a12f9550d2c56b9da48ae64e36a61166dfcc9
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-08-03 23:26:37 +0000

    Add tensorflow dependency headers and audio frontend dependencies
    The build is failing because the third-party download headers are missing.
    
    Invoke the download step from the tensorflow build to make them available
    for the micropython build.
    
    We were also missing some setup for the audiofrontend module.

--- ci/build_micropython_esp32.sh
+++ ci/build_micropython_esp32.sh
@@ -19,6 +19,22 @@ set -e
 
 BASE_DIR=/opt/tflite-micro-micropython
 
+cd $BASE_DIR/tensorflow
+
+# need to run the downloads again so the headers are available
+make -f tensorflow/lite/micro/tools/make/Makefile third_party_downloads 
+
+# need to download the kissft dependency.
+# make -f tensorflow/lite/micro/tools/make/Makefile generate_hello_world_make_project
+
+
+cd $BASE_DIR/micropython-modules/audio_frontend
+
+# at the moment micropython esp32 port doesn't work with .cc extension c++ files only .cpp
+# so copy to rename the line ending
+cp $BASE_DIR/tensorflow/tensorflow/lite/experimental/microfrontend/lib/fft.cc fft.cpp
+cp $BASE_DIR/tensorflow/tensorflow/lite/experimental/microfrontend/lib/fft_util.cc fft_util.cpp
+
 cd $BASE_DIR/micropython
 
 pwd
