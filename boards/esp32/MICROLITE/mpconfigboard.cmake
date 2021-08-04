c7b85e103c5f5a7047ef8469ef42780653a23a37
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-08-04 17:05:58 -0400

    Remove spi ram settings from microlite board
    The microlite board is supposed to be GENERIC plus the tensorflow
    related modules.
    
    So it was a mistake to enable spiram.

--- boards/esp32/MICROLITE/mpconfigboard.cmake
+++ boards/esp32/MICROLITE/mpconfigboard.cmake
@@ -4,7 +4,6 @@ set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.base
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
     ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
-    ${MICROPY_PORT_DIR}/boards/sdkconfig.spiram
     ${MICROPY_BOARD_DIR}/sdkconfig.partition
 
 )
