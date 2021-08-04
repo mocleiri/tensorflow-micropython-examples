3f70d831a3d7417dc765a6b0c274ad8e31270f4b
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2021-08-04 00:28:13 -0400

    Fix variable name of MICROPY_BOARD_DIR which was misspelled.

--- boards/esp32/MICROLITE/mpconfigboard.cmake
+++ boards/esp32/MICROLITE/mpconfigboard.cmake
@@ -3,7 +3,7 @@ set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
     ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
     ${MICROPY_PORT_DIR}/boards/sdkconfig.spiram
-    ${MICROPY_BOARDS_DIR}/sdkconfig.partition
+    ${MICROPY_BOARD_DIR}/sdkconfig.partition
 
 )
 if(NOT MICROPY_FROZEN_MANIFEST)
