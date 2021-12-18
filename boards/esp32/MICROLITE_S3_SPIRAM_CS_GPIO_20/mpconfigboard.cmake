59e6194d5ec95500de13b9218b00dba526689bad
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2021-12-17 23:27:20 -0500

    esp32s3 custom: remove sdkconfig.usb to have repl over UART

--- boards/esp32/MICROLITE_S3_SPIRAM_CS_GPIO_20/mpconfigboard.cmake
+++ boards/esp32/MICROLITE_S3_SPIRAM_CS_GPIO_20/mpconfigboard.cmake
@@ -3,7 +3,6 @@ set(IDF_TARGET esp32s3)
 set(SDKCONFIG_DEFAULTS
 
     ${MICROPY_PORT_DIR}/boards/sdkconfig.base
-    ${MICROPY_PORT_DIR}/boards/sdkconfig.usb
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
     ${MICROPY_PORT_DIR}/boards/sdkconfig.spiram_sx
     ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
