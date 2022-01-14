899312120b88b107c079464173073eca0d95b10d
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2022-01-14 14:54:42 -0500

    stm32: add the configuration for F446RE
    This turns off almost all of ulab and down to just the tflm ops for
    hello-world and micro-speech.

---
+++ boards/stm32/NUCLEO_F446RE_MICROLITE/tflm_ops.h
@@ -0,0 +1,191 @@
+// must have at least one op enabled.
+// need to leave one enabled
+// #define MICROLITE_OP_ABS (1)
+
+// changed to ops shown from netron for hello-world and micro-speech.
+
+//
+// micro-speech
+// #define MICROLITE_OP_CONV_2D (1)
+
+// hello-world
+// micro-speech
+// #define MICROLITE_OP_FULLY_CONNECTED   (1)
+
+// hello-world
+// #define MICROLITE_OP_RELU   (1)
+
+
+// micro-speech
+// #define MICROLITE_OP_SOFTMAX   (1)
+
+// -- 
+
+#define MICROLITE_OP_ABS (0)
+
+#define MICROLITE_OP_ADD (0)
+
+#define MICROLITE_OP_ADD_N (0)
+
+#define MICROLITE_OP_ARG_MAX (0)
+
+#define MICROLITE_OP_ARG_MIN (0)
+
+#define MICROLITE_OP_AVERAGE_POOL_2D (0)
+
+#define MICROLITE_OP_BATCH_TO_SPACE_ND (0)
+
+#define MICROLITE_OP_CAST (0)
+
+#define MICROLITE_OP_CEIL (0)
+
+#define MICROLITE_OP_CIRCULAR_BUFFER (0)
+
+#define MICROLITE_OP_CONCATENATION (0)
+
+// micro-speech
+#define MICROLITE_OP_CONV_2D (1)
+
+#define MICROLITE_OP_COS (0)
+
+#define MICROLITE_OP_CUMSUM   (0)
+
+#define MICROLITE_OP_DEPTH_TO_SPACE (0)
+
+#define MICROLITE_OP_DEPTHWISE_CONV_2D   (0)
+
+#define MICROLITE_OP_DEQUANTIZE   (0)
+
+#define MICROLITE_OP_DETECTION_POSTPROCESS   (0)
+
+#define MICROLITE_OP_ELU   (0)
+
+#define MICROLITE_OP_EQUAL   (0)
+
+#define MICROLITE_OP_ETHOSU   (0)
+
+#define MICROLITE_OP_EXP   (0)
+
+#define MICROLITE_OP_EXPAND_DIMS (0)
+
+#define MICROLITE_OP_FILL   (0)
+
+#define MICROLITE_OP_FLOOR   (0)
+
+#define MICROLITE_OP_FLOOR_DIV   (0)
+
+#define MICROLITE_OP_FLOOR_MOD   (0)
+
+// hello-world
+// micro-speech
+#define MICROLITE_OP_FULLY_CONNECTED   (1)
+
+#define MICROLITE_OP_GATHER   (0)
+
+#define MICROLITE_OP_GATHER_ND   (0)
+
+#define MICROLITE_OP_GREATER   (0)
+
+#define MICROLITE_OP_GREATER_EQUAL   (0)
+
+#define MICROLITE_OP_HARD_SWISH   (0)
+
+#define MICROLITE_OP_IF   (0)
+
+#define MICROLITE_OP_L2_NORMALIZATION   (0)
+
+#define MICROLITE_OP_L2_POOL_2D   (0)
+
+#define MICROLITE_OP_LEAKY_RELU   (0)
+
+#define MICROLITE_OP_LESS   (0)
+
+#define MICROLITE_OP_LESS_EQUAL   (0)
+
+#define MICROLITE_OP_LOG   (0)
+
+#define MICROLITE_OP_LOGICAL_AND (0)
+
+#define MICROLITE_OP_LOGICAL_NOT   (0)
+
+#define MICROLITE_OP_LOGICAL_OR   (0)
+
+#define MICROLITE_OP_LOGISTIC   (0)
+
+#define MICROLITE_OP_MAXIMUM   (0)
+
+#define MICROLITE_OP_MAX_POOL_2D   (0)
+
+#define MICROLITE_OP_MEAN   (0)
+
+#define MICROLITE_OP_MINIMUM   (0)
+
+#define MICROLITE_OP_MUL   (0)
+
+#define MICROLITE_OP_NEG   (0)
+
+#define MICROLITE_OP_NOT_EQUAL   (0)
+
+#define MICROLITE_OP_PACK   (0)
+
+#define MICROLITE_OP_PAD   (0)
+
+#define MICROLITE_OP_PADV2   (0)
+
+#define MICROLITE_OP_PRELU   (0)
+
+#define MICROLITE_OP_QUANTIZE   (0)
+
+#define MICROLITE_OP_REDUCE_MAX   (0)
+
+// hello-world
+#define MICROLITE_OP_RELU   (1)
+
+#define MICROLITE_OP_RELU6   (0)
+
+#define MICROLITE_OP_RESHAPE   (0)
+
+#define MICROLITE_OP_RESIZE_BILINEAR   (0)
+
+#define MICROLITE_OP_RESIZE_NEAREST_NEIGHBOR   (0)
+
+#define MICROLITE_OP_ROUND   (0)
+
+#define MICROLITE_OP_RSQRT   (0)
+
+#define MICROLITE_OP_SHAPE   (0)
+
+#define MICROLITE_OP_SIN   (0)
+
+// micro-speech
+#define MICROLITE_OP_SOFTMAX   (1)
+
+#define MICROLITE_OP_SPACE_TO_BATCH_ND   (0)
+
+#define MICROLITE_OP_SPACE_TO_DEPTH   (0)
+
+#define MICROLITE_OP_SPLIT   (0)
+
+#define MICROLITE_OP_SPLIT_V   (0)
+
+#define MICROLITE_OP_SQUEEZE   (0)
+
+#define MICROLITE_OP_SQRT   (0)
+
+#define MICROLITE_OP_SQUARE   (0)
+
+#define MICROLITE_OP_STRIDED_SLICE (0)
+
+#define MICROLITE_OP_SUB (0)
+
+#define MICROLITE_OP_SVDF (0)
+
+#define MICROLITE_OP_TANH (0)
+
+#define MICROLITE_OP_TRANSPOSE_CONV (0)
+
+#define MICROLITE_OP_TRANSPOSE (0)
+
+#define MICROLITE_OP_UNPACK (0)
+
+#define MICROLITE_OP_ZEROS_LIKE (0)
