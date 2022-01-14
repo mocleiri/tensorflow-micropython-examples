8f2ebdc452ed25e9ca09563a5189804ca2872ffe
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2022-01-14 16:07:52 -0500

    tflm: update to latest upstream
    I'm staying on my fork of tflm for the moment but I did sync to the
    latest upstream version.
    
    This brings in the api change for converting tflm .cc files into .cpp
    files.

--- micropython-modules/microlite/prepare-tflm-unix.sh
+++ micropython-modules/microlite/prepare-tflm-unix.sh
@@ -6,4 +6,4 @@ rm -rf ./tensorflow/lite/micro/tools/make/gen
 rm -rf ../micropython-modules/microlite/tflm
 
 python3 ./tensorflow/lite/micro/tools/project_generation/create_tflm_tree.py \
-          --examples micro_speech --rename-cc-to-cpp ../micropython-modules/microlite/tflm
+          --examples micro_speech --rename_cc_to_cpp ../micropython-modules/microlite/tflm
