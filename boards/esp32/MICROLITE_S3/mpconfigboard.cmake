2d0948b542c6c5bee92b2bb9e383fec59a1358f1
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2022-01-19 00:18:35 -0500

    Remove usb support from s3 boards
    It may be idf related and can be restored later.  At the moment no one
    can use the images without disabling usb.

--- boards/esp32/MICROLITE_S3/mpconfigboard.cmake
+++ boards/esp32/MICROLITE_S3/mpconfigboard.cmake
@@ -2,7 +2,6 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.base
-    ${MICROPY_PORT_DIR}/boards/sdkconfig.usb
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
     ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
     ${MICROPY_BOARD_DIR}/sdkconfig.board
