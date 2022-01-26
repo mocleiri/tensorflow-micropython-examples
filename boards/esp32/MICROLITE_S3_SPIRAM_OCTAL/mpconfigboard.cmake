f97bb18ab032c240d9ab72966bd3d0c5ebf45030
TGiles1998 <90575567+TGiles1998@users.noreply.github.com>
2022-01-26 13:26:10 +0000

    esp32s3: Added Seperate Octal Build

---
+++ boards/esp32/MICROLITE_S3_SPIRAM_OCTAL/mpconfigboard.cmake
@@ -0,0 +1,21 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.base
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.spiram_sx
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
+    ${MICROPY_BOARD_DIR}/sdkconfig.board
+    ${MICROPY_BOARD_DIR}/sdkconfig-8m.partition
+)
+
+message (STATUS "mpconfigboard.cmake: PROJECT_DIR=${PROJECT_DIR}")
+
+set(USER_C_MODULES
+    ${PROJECT_DIR}/micropython-modules/micropython.cmake
+)
+
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+endif()
