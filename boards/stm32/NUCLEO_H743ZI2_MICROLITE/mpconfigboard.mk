a6c13de2f525dbe8da15192f1980cad8e1a55cc6
Michael O'Cleirigh <michael.ocleirigh@rivulet.ca>
2022-02-23 22:53:35 -0500

    stm32: Fix Nucleo H743ZI2 Microlite build
    Set MICROPY_ROM_TEXT_COMPRESSION = 0 as this allows the firmware to fit.

--- boards/stm32/NUCLEO_H743ZI2_MICROLITE/mpconfigboard.mk
+++ boards/stm32/NUCLEO_H743ZI2_MICROLITE/mpconfigboard.mk
@@ -2,5 +2,10 @@ include boards/NUCLEO_H743ZI/mpconfigboard.mk
 
 USER_C_MODULES=../../../micropython-modules
 
+# when enabled this flag blows up the firware size and it won't fit
+# its a bit counter intuitive but keep this disabled to be able to git microlite into
+# flash
+MICROPY_ROM_TEXT_COMPRESSION = 0
+
 
 
