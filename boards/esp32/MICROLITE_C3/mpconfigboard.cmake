dc1a9392cf21023c7dd1d91bea2f1ff26b7f1f28
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2022-12-01 23:23:27 -0500

    Fix paths to sdk config files
    Also copy the micropython/ports/esp32/paritions.csv file into the MICROLITE_C3 and
    MICROLITE_C3_USB board directories.

--- boards/esp32/MICROLITE_C3/mpconfigboard.cmake
+++ boards/esp32/MICROLITE_C3/mpconfigboard.cmake
@@ -1,8 +1,9 @@
 set(IDF_TARGET esp32c3)
 
 set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.ble
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.base
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
 )
 
 set(USER_C_MODULES
--- boards/esp32/MICROLITE_C3_USB/mpconfigboard.cmake
+++ boards/esp32/MICROLITE_C3_USB/mpconfigboard.cmake
@@ -1,9 +1,9 @@
 set(IDF_TARGET esp32c3)
 
 set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.ble
-    boards/GENERIC_C3_USB/sdkconfig.board
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.base
+    ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
+    ${MICROPY_PORT_DIR}/boards/GENERIC_C3_USB/sdkconfig.board
 )
 
 set(USER_C_MODULES
