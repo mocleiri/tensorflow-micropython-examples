3ed56eca75089fb9572e0633c1eb63148074a479
Michael O'Cleirigh <michael.ocleirigh@gmail.com>
2022-12-18 20:04:39 -0500

    Picked up the board header flag for enabling UART REPL for esp32 s3 and s2 boards
    This is most likely the issue blocking repl from working on esp32 s3 eye as reported in issue
    78.

--- boards/esp32/MICROLITE_S2_SPIRAM/mpconfigboard.h
+++ boards/esp32/MICROLITE_S2_SPIRAM/mpconfigboard.h
@@ -4,6 +4,9 @@
 #define MICROPY_PY_BLUETOOTH                (0)
 #define MICROPY_HW_ENABLE_SDCARD            (0)
 
+// Enable UART REPL for modules that have an external USB-UART and don't use native USB.
+#define MICROPY_HW_ENABLE_UART_REPL         (1)
+
 // these were from the um_tiny s2 double check if they match the ESP32S2DEVKITM1R
 // #define MICROPY_HW_I2C0_SCL (9)
 // #define MICROPY_HW_I2C0_SDA (8)
